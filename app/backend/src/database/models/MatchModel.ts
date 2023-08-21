70bf065e1875113cb56d576e49ec75943bf36f00
Giuseppe <giuseppebrandalise@gmail.com>
2023-08-20 22:57:34 -0300

    Lint

--- app/backend/src/database/models/MatchModel.ts
+++ app/backend/src/database/models/MatchModel.ts
@@ -1,73 +1,72 @@
 import {
-    DataTypes,
-    Model,
-    InferAttributes,
-    InferCreationAttributes,
-    CreationOptional,
-  } from 'sequelize';
-  import db from '.';
-  // import OtherModel from './OtherModel';
-  import TeamModel from './TeamModel';
-  
-  class MatchModel extends Model<InferAttributes<MatchModel>,
-  InferCreationAttributes<MatchModel>> {
-    declare id: CreationOptional<number>;
-    declare homeTeamId: number;
-    declare homeTeamGoals: number;
-    declare awayTeamId: number;
-    declare awayTeamGoals: number;
-    declare inProgress: boolean;
-  }
-  
-  MatchModel.init({
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    homeTeamId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    homeTeamGoals: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    awayTeamId: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    awayTeamGoals: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    inProgress: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-    },
-  }, {
-    sequelize: db,
-    modelName: 'matches',
-    tableName: 'matches',
-    timestamps: false,
-    underscored: true,
-  });
-  
-  /**
-    * `Workaround` para aplicar as associations em TS:
-    * Associations 1:N devem ficar em uma das instâncias de modelo
-    * */
-  
-  // OtherModel.belongsTo(MatchModel, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
-  // OtherModel.belongsTo(MatchModel, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
-  MatchModel.belongsTo(TeamModel, { foreignKey: 'homeTeamId', as: 'homeTeam' });
-  MatchModel.belongsTo(TeamModel, { foreignKey: 'awayTeamId', as: 'awayTeam' });
-  
-  // MatchModel.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
-  // MatchModel.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
-  MatchModel.hasMany(TeamModel, { foreignKey: 'homeTeamId', as: 'homeTeamMatch' });
-  MatchModel.hasMany(TeamModel, { foreignKey: 'awayTeamId', as: 'awayTeamMatch' });
-  
-  export default MatchModel;
-  
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+import TeamModel from './TeamModel';
+
+class MatchModel extends Model<InferAttributes<MatchModel>,
+InferCreationAttributes<MatchModel>> {
+  declare id: CreationOptional<number>;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+MatchModel.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  tableName: 'matches',
+  timestamps: false,
+  underscored: true,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(MatchModel, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(MatchModel, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+MatchModel.belongsTo(TeamModel, { foreignKey: 'homeTeamId', as: 'homeTeam' });
+MatchModel.belongsTo(TeamModel, { foreignKey: 'awayTeamId', as: 'awayTeam' });
+
+// MatchModel.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// MatchModel.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+MatchModel.hasMany(TeamModel, { foreignKey: 'homeTeamId', as: 'homeTeamMatch' });
+MatchModel.hasMany(TeamModel, { foreignKey: 'awayTeamId', as: 'awayTeamMatch' });
+
+export default MatchModel;
--- app/backend/src/database/models/UserModel.ts
+++ app/backend/src/database/models/UserModel.ts
@@ -1,63 +1,62 @@
 import {
-    DataTypes,
-    Model,
-    InferAttributes,
-    InferCreationAttributes,
-    CreationOptional,
-  } from 'sequelize';
-  import db from '.';
-  // import OtherModel from './OtherModel';
-  
-  class UserModel extends Model<InferAttributes<UserModel>,
-  InferCreationAttributes<UserModel>> {
-    declare id: CreationOptional<number>;
-    declare username: string;
-    declare role: string;
-    declare email: string;
-    declare password: string;
-  }
-  
-  UserModel.init({
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    role: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  }, {
-    sequelize: db,
-    modelName: 'users',
-    tableName: 'users',
-    timestamps: false,
-  });
-  
-  /**
-    * `Workaround` para aplicar as associations em TS:
-    * Associations 1:N devem ficar em uma das instâncias de modelo
-    * */
-  
-  // OtherModel.belongsTo(UserModel, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
-  // OtherModel.belongsTo(UserModel, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
-  
-  // UserModel.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
-  // UserModel.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
-  
-  export default UserModel;
-  
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class UserModel extends Model<InferAttributes<UserModel>,
+InferCreationAttributes<UserModel>> {
+  declare id: CreationOptional<number>;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+UserModel.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'users',
+  tableName: 'users',
+  timestamps: false,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(UserModel, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(UserModel, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// UserModel.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// UserModel.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default UserModel;
